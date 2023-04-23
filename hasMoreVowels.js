95346a535dc140e13fbbca0153ae6791f708ae24
Jaclyn Foster <jaclynfoster89@gmail.com>
2023-04-23 17:45:02 -0500

    Has more vowels challenge

---
+++ hasMoreVowels.js
@@ -0,0 +1,42 @@
+// Write a function called hasMoreVowels that takes in one argument, word. When the function is called, 
+//return true if that word contains more vowels than non-vowels; otherwise, return false. 
+//The word will always be a single word, without any punctuation or spaces.
+// It will contain only uppercase and/or lowercase letters.
+
+// If the phrase is over half vowels, it should return true:
+// hasMoreVowels('moose')
+// // true
+// If it’s half vowels (or less), it’s false:
+
+// hasMoreVowels('mice')
+// // false
+
+// hasMoreVowels('graph')
+// // false
+// Don’t consider “y” as a vowel:
+
+// hasMoreVowels('yay')
+// // false
+// Uppercase vowels are still vowels:
+
+// hasMoreVowels('Aal')
+// // true
+
+const hasMoreVowels = (word) => {
+    let vowels = ["a", "e", "i", "o", "u"]
+    let vowelCount = 0
+for (let letter of word) {
+    if (vowels.includes(letter)) {
+        vowelCount++
+    }
+}
+console.log('vowel count: ', vowelCount)
+    if(vowelCount >= word.length / 2) {
+        return true
+    } else {
+        return false
+    }
+
+}
+
+console.log(hasMoreVowels("dig"))
